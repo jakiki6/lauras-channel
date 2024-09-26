412a3fa20063d1860fe2e3420c8f5208a36c15fc
Jakob Kirsch <jakob.kirsch@web.de>
2024-09-26 17:22:07 +0200

    bump nomadnet

--- laura/packages/nomadnet.scm
+++ laura/packages/nomadnet.scm
@@ -1,23 +1,25 @@
 (define-module (laura packages nomadnet)
   #:use-module (guix packages)
   #:use-module (guix download)
+  #:use-module (guix git-download)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix build-system python)
   #:use-module (guix build-system pyproject)
   #:use-module (gnu packages python-xyz)
-  #:use-module (gnu packages python-crypto)
-)
+  #:use-module (gnu packages python-crypto))
 
 (define-public python-rns
   (package
     (name "python-rns")
-    (version "0.7.5")
+    (version "0.8.0")
     (source
      (origin
-       (method url-fetch)
-       (uri (pypi-uri "rns" version))
+       (method git-fetch)
+       (uri (git-reference
+              (url "https://github.com/markqvist/Reticulum")
+              (commit version)))
        (sha256
-        (base32 "175xwc4camk738803k10848s658sznxly6a778lii4iinziaxd94"))))
+        (base32 "19szbapwic1qxms1aw0ynak72fx7xj35jfwgszrhwqviixzzd093"))))
     (build-system pyproject-build-system)
     (propagated-inputs (list python-cryptography python-pyserial))
     (arguments `(#:tests? #f #:phases (modify-phases %standard-phases (delete 'sanity-check))))
@@ -32,16 +34,18 @@ packet radio, @code{WiFi} and everything in between.")
 (define-public python-lxmf
   (package
     (name "python-lxmf")
-    (version "0.4.4")
+    (version "0.5.3")
     (source
      (origin
-       (method url-fetch)
-       (uri (pypi-uri "lxmf" version))
+       (method git-fetch)
+       (uri (git-reference
+              (url "https://github.com/markqvist/LXMF")
+              (commit version)))
        (sha256
-        (base32 "0nm3j96qjxa6px2qh53mr571azsc8pn9k3z55mv3ybznz8pcz3gq"))))
+        (base32 "1bsk2c73lz9vf5q36mmkzdwjcmgl6igd97n8scign6ri70gddhki"))))
     (build-system pyproject-build-system)
     (propagated-inputs (list python-rns))
-    (arguments `(#:tests? #f))
+    (arguments `(#:tests? #f #:phases (modify-phases %standard-phases (delete 'sanity-check))))
     (home-page "https://github.com/markqvist/lxmf")
     (synopsis "Lightweight Extensible Message Format for Reticulum")
     (description "Lightweight Extensible Message Format for Reticulum.")
@@ -50,13 +54,15 @@ packet radio, @code{WiFi} and everything in between.")
 (define-public nomadnet
   (package
     (name "nomadnet")
-    (version "0.4.9")
+    (version "0.5.3")
     (source
-     (origin
-       (method url-fetch)
-       (uri (pypi-uri "nomadnet" version))
+    (origin
+      (method git-fetch)
+      (uri (git-reference
+             (url "https://github.com/markqvist/NomadNet")
+             (commit version)))
        (sha256
-        (base32 "00rycinwm7nlw0nzdnwk843r9ydic8hl3zr75bwffldarsh24ibs"))))
+        (base32 "0nzjflxqv1vy16gsbj79pj6ab81iw8x8qmnjc3cjdf327mwx3g7c"))))
     (build-system pyproject-build-system)
     (propagated-inputs (list python-lxmf python-qrcode python-rns python-urwid))
     (arguments `(#:tests? #f #:phases (modify-phases %standard-phases (delete 'sanity-check))))
