32423e4fe16653ffaa3acea00464c225a047449c
Jakob Kirsch <jakob.kirsch@web.de>
2024-10-06 19:55:01 +0200

    add activate-linux

--- README.md
+++ README.md
@@ -26,6 +26,7 @@ Software that I've ported so far:
 * [cerca](https://github.com/cblgh/cerca)
 * [sbctl](https://github.com/Foxboron/sbctl)
 * [Ricochet Refresh](https://github.com/blueprint-freespeech/ricochet-refresh)
+* [activate-linux](https://github.com/Kljunas2/activate-linux)
 
 Things that are still in progress:
 * gradle (see [this discussion](https://discuss.gradle.org/t/building-gradle-from-pure-source-without-any-bootstrap-binaries/19398))
--- laura/packages/utils.scm
+++ laura/packages/utils.scm
@@ -38,6 +38,7 @@
   #:use-module (gnu packages version-control)
   #:use-module (gnu packages qt)
   #:use-module (gnu packages xdisorg)
+  #:use-module (gnu packages gtk)
   #:use-module (guix build utils)
   #:use-module (guix build-system cmake)
   #:use-module (guix build-system go)
@@ -872,3 +873,29 @@ needs to be signed in the boot chain.")
     (synopsis "Anonymous peer-to-peer instant messaging")
     (description "Ricochet Refresh is an instant messenger where no one knows your identity, who you're talking to, or what you're talking about.")
     (license license:bsd-3)))
+
+(define-public activate-linux
+  (package
+    (name "activate-linux")
+    (version "1.0")
+    (source
+      (origin
+        (method git-fetch)
+        (uri (git-reference
+              (url "https://github.com/Kljunas2/activate-linux")
+              (commit "8521a85e8b30229635dea58ab053f2b7d674d242")))
+        (file-name (git-file-name name version))
+        (sha256 (base32 "0g4w73jhinw9kx9kixsyjy29q0p1jqhgw4yycmfghc0shyiv7vyh"))))
+    (build-system gnu-build-system)
+    (native-inputs (list pkg-config))
+    (inputs (list gtk-layer-shell gtk+))
+    (arguments
+      `(#:tests? #f
+        #:phases (modify-phases %standard-phases
+          (delete 'configure)
+          (replace 'install (lambda* (#:key outputs #:allow-other-keys)
+            (install-file "bin/activate-linux" (string-append (assoc-ref outputs "out") "/bin")))))))
+    (home-page "https://github.com/Kljunas2/activate-linux")
+    (synopsis "Activate Linux watermark for Wayland")
+    (description "The \"Activate Windows\" watermark ported to Linux with Gtk Layer Shell")
+    (license license:expat)))
