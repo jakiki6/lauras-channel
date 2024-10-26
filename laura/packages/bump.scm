efab07178cb74f24830b336ba7ff326e48e412dc
Jakob Kirsch <jakob.kirsch@web.de>
2024-10-26 22:44:07 +0200

    fix yosys

--- laura/packages/bump.scm
+++ laura/packages/bump.scm
@@ -2,7 +2,8 @@
   #:use-module (guix packages)
   #:use-module (guix download)
   #:use-module (guix git-download)
-  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module ((guix licenses)
+                #:prefix license:)
   #:use-module (guix build-system gnu)
   #:use-module (guix build-system cmake)
   #:use-module (guix gexp)
@@ -23,6 +24,7 @@
   #:use-module (gnu packages qt)
   #:use-module (gnu packages glib)
   #:use-module (gnu packages freedesktop)
+  #:use-module (gnu packages compression)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages xdisorg)
   #:use-module (gnu packages gcc))
@@ -31,18 +33,21 @@
   (package
     (inherit squirrel)
     (version "3.2")
-    (source (origin
-              (method url-fetch)
-              (uri (string-append "mirror://sourceforge/squirrel/squirrel3/"
-                                  "squirrel " version " stable/squirrel_"
-                                  (string-join (string-split version #\.) "_")
-                                  "_stable.tar.gz"))
-              (file-name (string-append "squirrel-" version ".tar.gz"))
-              (patches (list (search-patch "laura/packages/patches/squirrel-no-cmake-files.patch")))
-              (patch-flags '("-p1" "--binary"))
-              (sha256
-               (base32
-                "1nw1ghr4nxsvpk5bm9q0arrx4zrjpq5bai5sc17vj90by19187r1"))))))
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "mirror://sourceforge/squirrel/squirrel3/"
+                           "squirrel "
+                           version
+                           " stable/squirrel_"
+                           (string-join (string-split version #\.) "_")
+                           "_stable.tar.gz"))
+       (file-name (string-append "squirrel-" version ".tar.gz"))
+       (patches (list (search-patch
+                       "laura/packages/patches/squirrel-no-cmake-files.patch")))
+       (patch-flags '("-p1" "--binary"))
+       (sha256
+        (base32 "1nw1ghr4nxsvpk5bm9q0arrx4zrjpq5bai5sc17vj90by19187r1"))))))
 
 (define iverilog-fixed
   (package
@@ -50,18 +55,18 @@
     (inputs (list zlib))))
 
 (define-public yosys-0.46
-   (package
-     (inherit yosys)
-     (name "yosys")
-     (version "0.46")
-     (source (origin
-               (method git-fetch)
-               (uri (git-reference
-                     (url "https://github.com/YosysHQ/yosys")
-                     (commit version)))
-               (sha256
-                (base32
-                 "1zj7vbpy6v1wn4p5cjs4hdjd467a1j1aj2qhs148bl2s6mzq3p86"))
-               (file-name (git-file-name name version))))
-    (inputs (modify-inputs (package-inputs yosys)
-      (replace "iverilog" iverilog-fixed)))))
+  (package
+    (inherit yosys)
+    (name "yosys")
+    (version "0.46")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/YosysHQ/yosys")
+             (commit version)))
+       (sha256
+        (base32 "1zj7vbpy6v1wn4p5cjs4hdjd467a1j1aj2qhs148bl2s6mzq3p86"))
+       (file-name (git-file-name name version))))
+    (native-inputs (modify-inputs (package-native-inputs yosys)
+                     (replace "iverilog" iverilog-fixed)))))
