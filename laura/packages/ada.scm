1215aa46daeed1cb3665a7e526ac3cc59e3f219b
Jakob Kirsch <jakob.kirsch@web.de>
2025-02-07 18:48:59 +0100

    this is cleaner

--- laura/packages/ada.scm
+++ laura/packages/ada.scm
@@ -3,7 +3,7 @@
   #:use-module (guix download)
   #:use-module (guix git-download)
   #:use-module (guix gexp)
-  #:use-module (guix build-system meson)
+  #:use-module (guix build-system trivial)
   #:use-module (guix build-system gnu)
   #:use-module (gnu packages cmake)
   #:use-module ((guix licenses)
@@ -36,6 +36,43 @@
   #:use-module (gnu packages gcc)
   #:use-module (guix-science packages electronics))
 
+(define gnat-patched
+  (package
+    (inherit gnat)
+    (source
+     #f)
+    (build-system trivial-build-system)
+    (native-inputs (list sed))
+    (inputs (list gnat))
+    (arguments
+     (list
+      #:modules '((guix build utils))
+      #:builder #~(begin
+                    (use-modules (guix build utils))
+                    (copy-recursively #$gnat
+                                      (assoc-ref %outputs "out")
+                                      #:keep-permissions? #t)
+                    (chdir (assoc-ref %outputs "out"))
+                    (make-file-writable "bin")
+                    (make-file-writable "lib/gcc/x86_64-pc-linux-gnu/14.2.0/plugin/include/config/i386")
+                    (map (lambda (x)
+                           (begin
+                             (format #t "Patching ~a\n" x)
+                             (make-file-writable x)
+                             (system (string-append #$sed
+                                      "/bin/sed -i -e 's|/lib64/ld-linux-x86-64.so.2|/tmp/64ld-linux-x86-64.so.2|g' "
+                                      x))))
+                         (list "bin/c++"
+                          "bin/cpp"
+                          "bin/gcc"
+                          "bin/g++"
+                          "bin/gp-display-html"
+                          "bin/x86_64-pc-linux-gnu-c++"
+                          "bin/x86_64-pc-linux-gnu-g++"
+                          "bin/x86_64-pc-linux-gnu-gcc"
+                          "bin/x86_64-pc-linux-gnu-gcc-14.2.0"
+                          "lib/gcc/x86_64-pc-linux-gnu/14.2.0/plugin/include/config/i386/linux64.h")))))))
+
 (define-public gnat-i686
   (package
     (name "gnat-i686")
@@ -47,37 +84,16 @@
        (sha256
         (base32 "1j9wdznsp772q15w1kl5ip0gf0bh8wkanq2sdj12b7mzkk39pcx7"))))
     (build-system gnu-build-system)
-    (native-inputs (list gnat))
-    (inputs (list gcc-14 gmp mpfr mpc isl glibc))
+    (native-inputs (list gnat-patched))
+    (inputs (list gmp mpfr mpc isl))
     (arguments
      (list
-      #:parallel-build? #false
       #:phases #~(modify-phases %standard-phases
                    (add-before 'configure 'install-ld
-                     (lambda* (#:key inputs #:allow-other-keys)
-                       (begin
-                         (use-modules (ice-9 string-fun))
-                         (system (string-append
-                                  "gcc -dumpspecs | sed -e 's/\\/lib64\\/ld-linux-x86-64.so.2/"
-                                  (string-replace-substring #$glibc "/" "\\/")
-                                  "\\/lib\\/ld-linux-x86-64.so.2/g' > /tmp/specs"))
-                         (mkdir "/tmp/bin")
-                         (system (string-append "printf \"#!"
-                                  #$bash
-                                  "/bin/sh\\nexport PATH=\\$_PATH\\ngcc -specs /tmp/specs \\\"\\$@\\\"\\n\" > /tmp/bin/gcc"))
-                         (chmod "/tmp/bin/gcc" #o755)
-                         (system (string-append "printf \"#!"
-                                  #$bash
-                                  "/bin/sh\\nexport PATH=\\$_PATH\\ng++ -specs /tmp/specs \\\"\\$@\\\"\\n\" > /tmp/bin/g++"))
-                         (chmod "/tmp/bin/g++" #o755)
-                         (system (string-append "ln -s " #$gnat "/bin/ar /tmp/bin/x86_64-linux-gnu-ar"))
-                         (setenv "_PATH"
-                                 (getenv "PATH"))
-                         (setenv "PATH"
-                                 (string-append "/tmp/bin:"
-                                                (getenv "PATH")))
-                         (setenv "CPLUS_INCLUDE_PATH" (string-append #$gcc "/include/c++/x86_64-unknown-linux-gnu:" (getenv "CPLUS_INCLUDE_PATH")))
-))))
+                     (lambda _
+                       (symlink (string-append #$glibc
+                                               "/lib/ld-linux-x86-64.so.2")
+                                "/tmp/64ld-linux-x86-64.so.2"))))
       #:configure-flags #~(list "--enable-languages=c,ada,c++"
                                 "--enable-libstdcxx"
                                 "--enable-libstdcxx-threads"
@@ -90,7 +106,6 @@
                                 "--enable-threads=posix"
                                 "--with-gnu-ld"
                                 "--with-gnu-as"
-                                "--host=x86_64-linux-gnu"
                                 "--target=i686-linux-gnu")))
     (home-page "https://www.gnu.org/software/gnat/")
     (synopsis "GNU GNAT for i686")
