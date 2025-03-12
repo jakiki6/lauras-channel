4fe54e0b0807b2a8467bc9c76088e364d127495b
Jakob Kirsch <jakob.kirsch@web.de>
2025-03-12 21:29:51 +0100

    add ollama

---
+++ laura/packages/ai.scm
@@ -0,0 +1,81 @@
+(define-module (laura packages ai)
+  #:use-module (guix)
+  #:use-module ((guix licenses)
+                #:prefix license:)
+  #:use-module (gnu packages)
+  #:use-module (guix build-system go)
+  #:use-module (guix git-download)
+  #:use-module (gnu packages golang)
+  #:use-module (gnu packages golang-build)
+  #:use-module (gnu packages golang-xyz)
+  #:use-module (gnu packages golang-maths)
+  #:use-module (gnu packages golang-check)
+  #:use-module (gnu packages golang-web)
+  #:use-module (laura packages go-common))
+
+(define-public ollama
+  (package
+    (name "ollama")
+    (version "0.6.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/ollama/ollama")
+             (commit "v0.6.0")))
+       (file-name (git-file-name name version))
+       (modules '((guix build utils)))
+       (snippet #~(map (lambda x
+                         (substitute* x
+                           (("github.com/pdevine/tensor")
+                            "gorgonia.org/tensor")))
+                       (list "convert/convert_gemma.go"
+                             "convert/convert_gemma2_adapter.go"
+                             "convert/convert_llama.go"
+                             "convert/convert_llama_adapter.go"
+                             "go.mod"
+                             "go.sum")))
+       (patches (search-patches
+                 "laura/packages/patches/ollama-use-go-1.23.patch"))
+       (sha256
+        (base32 "0a4vj6fk613lirzxg5z67dqjayx11qfm2s1n8i47wv6k38ng7jf5"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:tests? #f
+      #:go go-1.23
+      #:unpack-path "github.com/ollama/ollama"
+      #:import-path "github.com/ollama/ollama"))
+    (propagated-inputs (list go-std-1.23
+                             go-google-golang-org-protobuf
+                             go-golang-org-x-text
+                             go-golang-org-x-term
+                             go-golang-org-x-sys
+                             go-golang-org-x-exp
+                             go-golang-org-x-crypto
+                             go-github-com-gin-contrib-cors
+                             go-golang-org-x-tools
+                             go-golang-org-x-image
+                             go-gorgonia-org-tensor
+                             go-github-com-nlpodyssey-gopickle
+                             go-github-com-mattn-go-runewidth
+                             go-github-com-google-go-cmp
+                             go-github-com-emirpasic-gods
+                             go-github-com-dlclark-regexp2-v1.11.5
+                             go-github-com-d4l3k-go-bfloat16
+                             go-github-com-agnivade-levenshtein
+                             go-golang-org-x-sync
+                             go-github-com-x448-float16
+                             go-github-com-stretchr-testify
+                             go-github-com-spf13-cobra
+                             go-github-com-olekukonko-tablewriter
+                             go-github-com-google-uuid
+                             go-github-com-gin-gonic-gin
+                             go-github-com-containerd-console
+
+                             ))
+    (home-page "https://ollama.com")
+    (synopsis "Get up and running with large language models.")
+    (description
+     "Get up and running with Llama 3.3, DeepSeek-R1, Phi-4, Gemma 3, and other large language models.")
+    (license license:expat)))
--- laura/packages/go-common.scm
+++ laura/packages/go-common.scm
@@ -16,6 +16,8 @@
   #:use-module (gnu packages prometheus)
   #:use-module (gnu packages golang-web)
   #:use-module (gnu packages golang-xyz)
+  #:use-module (gnu packages golang-maths)
+  #:use-module (gnu packages golang-compression)
   #:use-module (gnu packages tls))
 
 (define-public go-github-com-rogpeppe-clock
@@ -812,3 +814,463 @@ Gomega error detection whenever trying to be @code{Successful()}.")
      "Package enumflag supplements the Golang CLI flag handling packages spf13/cobra
 and spf13/pflag with enumeration flags.")
     (license license:asl2.0)))
+
+(define-public go-github-com-d4l3k-go-bfloat16
+  (package
+    (name "go-github-com-d4l3k-go-bfloat16")
+    (version "0.0.0-20211005043715-690c3bdd05f1")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/d4l3k/go-bfloat16")
+             (commit (go-version->git-ref version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1bshygdr5lcagznrh349r53whqhlg870j484zpsi3f7ilqv08rvy"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "github.com/d4l3k/go-bfloat16"))
+    (home-page "https://github.com/d4l3k/go-bfloat16")
+    (synopsis "go-bfloat16")
+    (description "BFloat16 conversion utilities for Go/Golang.")
+    (license license:expat)))
+
+(define-public go-github-com-emirpasic-gods
+  (package
+    (name "go-github-com-emirpasic-gods")
+    (version "2.0.0-alpha")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/emirpasic/gods")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1qhjxv9rc1jjq57w84zz51lpx6baw3ywzj9ls491y1gkg5ycc765"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "github.com/emirpasic/gods"
+      #:unpack-path "github.com/emirpasic/gods"
+      #:phases
+      #~(modify-phases %standard-phases
+          (delete 'build)
+          (delete 'check))))
+    (home-page "https://github.com/emirpasic/gods")
+    (synopsis "GoDS (Go Data Structures)")
+    (description
+     "Implementation of various data structures and algorithms in Go.")
+    (license (list license:bsd-2 license:expat))))
+
+(define-public go-github-com-nlpodyssey-gopickle
+  (package
+    (name "go-github-com-nlpodyssey-gopickle")
+    (version "0.3.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/nlpodyssey/gopickle")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1fadbyq63i55g3k91knm7m1pl3j0krxdgpajrl78h27sl3mhnhal"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "github.com/nlpodyssey/gopickle"
+      #:phases
+      #~(modify-phases %standard-phases
+          (delete 'build)
+          (delete 'check))
+
+      ))
+    (propagated-inputs (list go-golang-org-x-text))
+    (home-page "https://github.com/nlpodyssey/gopickle")
+    (synopsis "GoPickle")
+    (description
+     "@code{GoPickle} is a Go library for loading Python's data serialized with
+@@code{pickle} and @code{PyTorch} module files.")
+    (license license:bsd-2)))
+
+(define-public go-github-com-apache-arrow-go-arrow
+  (package
+    (name "go-github-com-apache-arrow-go-arrow")
+    (version "0.0.0-20211112161151-bc219186db40")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/apache/arrow")
+             (commit (go-version->git-ref version
+                                          #:subdir "go/arrow"))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "03nh7c0i3y9rkkzw428knalkrlpb8syr459i00mwp072ijn8v4hx"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "github.com/apache/arrow/go/arrow"
+      #:unpack-path "github.com/apache/arrow"))
+    (propagated-inputs (list go-google-golang-org-protobuf
+                             go-google-golang-org-grpc
+                             go-gonum-org-v1-gonum
+                             go-golang-org-x-xerrors
+                             go-golang-org-x-exp
+                             go-github-com-stretchr-testify
+                             go-github-com-pierrec-lz4-v4
+                             go-github-com-klauspost-compress
+                             go-github-com-google-flatbuffers
+                             go-github-com-golang-protobuf))
+    (home-page "https://github.com/apache/arrow")
+    (synopsis #f)
+    (description "Package arrow provides an implementation of Apache Arrow.")
+    (license license:asl2.0)))
+
+(define-public go-github-com-chewxy-hm
+  (package
+    (name "go-github-com-chewxy-hm")
+    (version "1.0.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/chewxy/hm")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0f4qwg1q2lc9y64wrl9qxyimqnnandlqg78gn3yv4vsmyci025r7"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "github.com/chewxy/hm"))
+    (propagated-inputs (list go-github-com-pkg-errors go-github-com-xtgo-set
+                             go-github-com-stretchr-testify))
+    (home-page "https://github.com/chewxy/hm")
+    (synopsis "hm")
+    (description
+     "Package hm is a simple Hindley-Milner type inference system in Go.  It provides
+the necessary data structures and functions for creating such a system.")
+    (license license:expat)))
+
+(define-public go-github-com-google-flatbuffers
+  (package
+    (name "go-github-com-google-flatbuffers")
+    (version "25.2.10")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/google/flatbuffers")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1wcpaj4k9dwccb752pl8p54pqwajr51sxjym32q2bpm9ny6ib45v"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "github.com/google/flatbuffers"
+      #:phases
+      #~(modify-phases %standard-phases
+          (delete 'build)
+          (delete 'check))))
+    (home-page "https://github.com/google/flatbuffers")
+    (synopsis "FlatBuffers")
+    (description
+     "@@strong{@code{FlatBuffers}} is a cross platform serialization library
+architected for maximum memory efficiency.  It allows you to directly access
+serialized data without parsing/unpacking it first, while still having great
+forwards/backwards compatibility.")
+    (license license:asl2.0)))
+
+(define-public go-go4-org-unsafe-assume-no-moving-gc
+  (package
+    (name "go-go4-org-unsafe-assume-no-moving-gc")
+    (version "0.0.0-20231121144256-b99613f794b6")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/go4org/unsafe-assume-no-moving-gc")
+             (commit (go-version->git-ref version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "00ny3qha8k9nnx37ryvls2n5r7lw3bnldz6kwdmjxk8s19mxqim7"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "go4.org/unsafe/assume-no-moving-gc"))
+    (home-page "https://go4.org/unsafe/assume-no-moving-gc")
+    (synopsis "go4.org/unsafe/assume-no-moving-gc")
+    (description
+     "Package go4.org/unsafe/assume-no-moving-gc exists so you can depend on it from
+unsafe code that wants to declare that it assumes that the Go runtime does not
+use a moving garbage collector.  Specifically, it asserts that the caller is
+playing stupid games with the addresses of heap-allocated values.  It says
+nothing about values that Go's escape analysis keeps on the stack.  Ensuring
+things aren't stack-allocated is the caller's responsibility.")
+    (license license:bsd-3)))
+
+(define-public go-github-com-chewxy-math32
+  (package
+    (name "go-github-com-chewxy-math32")
+    (version "1.11.1")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/chewxy/math32")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0i7jssi872mv7h4rc4y0xa88a0hsr03mydqyrd6mrm8n7q8rfml9"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "github.com/chewxy/math32"))
+    (home-page "https://github.com/chewxy/math32")
+    (synopsis "math32")
+    (description
+     "Package math32 provides basic constants and mathematical functions for float32
+types.")
+    (license license:bsd-2)))
+
+(define-public go-gorgonia-org-vecf32
+  (package
+    (name "go-gorgonia-org-vecf32")
+    (version "0.9.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/gorgonia/vecf32")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0jggbf98fbbip7znx5m4n2lqqsnw5kqycj3gcbs62ypirr1pp0m9"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "gorgonia.org/vecf32"))
+    (propagated-inputs (list go-github-com-stretchr-testify
+                             go-github-com-pmezard-go-difflib
+                             go-github-com-davecgh-go-spew
+                             go-github-com-chewxy-math32))
+    (home-page "https://gorgonia.org/vecf32")
+    (synopsis "vecf32")
+    (description
+     "Package vecf32 provides common functions and methods for slices of float32.")
+    (license license:expat)))
+
+(define-public go-gorgonia-org-vecf64
+  (package
+    (name "go-gorgonia-org-vecf64")
+    (version "0.9.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/gorgonia/vecf64")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0a8v65cy6gyh7ww2g8q4p6dmjhcd6k7lm7z8ly4vmi4k0vq1w187"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:tests? #f
+      #:import-path "gorgonia.org/vecf64"))
+    (propagated-inputs (list go-github-com-stretchr-testify
+                             go-github-com-pmezard-go-difflib
+                             go-github-com-davecgh-go-spew))
+    (home-page "https://gorgonia.org/vecf64")
+    (synopsis "vecf64")
+    (description
+     "Package vecf64 provides common functions and methods for slices of float64.")
+    (license license:expat)))
+
+(define-public go-github-com-gin-contrib-cors
+  (package
+    (name "go-github-com-gin-contrib-cors")
+    (version "1.7.3")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/gin-contrib/cors")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1hh1vl0xqadpnkrs0ji8g9d4m4gdqgkfm1jjry66gzk70zkrdybd"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "github.com/gin-contrib/cors"))
+    (propagated-inputs (list go-github-com-stretchr-testify
+                             go-github-com-gin-gonic-gin))
+    (home-page "https://github.com/gin-contrib/cors")
+    (synopsis "CORS gin's middleware")
+    (description "Gin middleware/handler to enable CORS support.")
+    (license license:expat)))
+
+; from https://git.sr.ht/~efraim/my-guix
+; licensed under the GPL 3
+(define-public go-google-golang-org-grpc
+  (package
+    (name "go-google-golang-org-grpc")
+    (version "1.43.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/grpc/grpc-go")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "10f9363yir4l7rnj5z897qk79si7913vsyzy4nw5xhxjhxsppji8"))))
+    (build-system go-build-system)
+    (arguments
+     '(#:import-path "google.golang.org/grpc"
+       #:phases (modify-phases %standard-phases
+                  (delete 'build)
+                  (delete 'check))))
+    (propagated-inputs (list go-google-golang-org-protobuf
+                             go-google-golang-org-genproto
+                             go-golang-org-x-text
+                             go-golang-org-x-sys
+                             ;; go-golang-org-x-oauth2
+                             go-golang-org-x-net
+                             ;; go-github-com-google-uuid
+                             ;; go-github-com-google-go-cmp
+                             go-github-com-golang-protobuf
+                             go-github-com-golang-glog
+                             ;; go-github-com-envoyproxy-go-control-plane
+                             ;; go-github-com-cncf-xds-go
+                             ;; go-github-com-cncf-udpa-go
+                             go-github-com-cespare-xxhash))
+    (home-page "https://google.golang.org/grpc")
+    (synopsis "gRPC-Go")
+    (description "Package grpc implements an RPC system called gRPC.")
+    (license license:asl2.0)))
+
+; see last comment
+(define-public go-google-golang-org-genproto
+  (package
+    (name "go-google-golang-org-genproto")
+    (version "0.0.0-20211223182754-3ac035c7e7cb")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/googleapis/go-genproto")
+             (commit (go-version->git-ref version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0fk3n2f6x3pyrfx2nn5ws88sp16yr42awc1n5dsfkzx47w9grczi"))))
+    (build-system go-build-system)
+    (arguments
+     '(#:import-path "google.golang.org/genproto"
+       #:phases (modify-phases %standard-phases
+                  (delete 'build)
+                  (delete 'check))))
+    (propagated-inputs (list ;go-google-golang-org-protobuf
+                             ;; go-google-golang-org-grpc
+                             ;; go-golang-org-x-text
+                             ;; go-golang-org-x-sys
+                             ;; go-golang-org-x-net
+                             ;; go-github-com-golang-protobuf
+                             ))
+    (home-page "https://google.golang.org/genproto")
+    (synopsis "Go generated proto packages")
+    (description
+     "This repository contains the generated Go packages for common protocol buffer
+types, and the generated @url{http://grpc.io,gRPC} code necessary for
+interacting with Google's gRPC APIs.")
+    (license license:asl2.0)))
+
+(define-public go-github-com-xtgo-set
+  (package
+    (name "go-github-com-xtgo-set")
+    (version "1.0.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/xtgo/set")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "148jb5f87lf7090jg8340f24r29818krydajkm75vpzylaw6yd8w"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "github.com/xtgo/set"))
+    (home-page "https://github.com/xtgo/set")
+    (synopsis "set")
+    (description
+     "Package set implements type-safe, non-allocating algorithms that operate on
+ordered sets.")
+    (license license:bsd-2)))
+
+(define-public go-gorgonia-org-tensor
+  (package
+    (name "go-gorgonia-org-tensor")
+    (version "0.9.24")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/gorgonia/tensor")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "13dk1gmplik5z8x27khgk5aja480znq4ryx3j0csp2j9vnwccpl3"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:tests? #f
+      #:import-path "gorgonia.org/tensor"))
+    (propagated-inputs (list go-gorgonia-org-vecf64
+                             go-gorgonia-org-vecf32
+                             go-gonum-org-v1-gonum
+                             go-go4-org-unsafe-assume-no-moving-gc
+                             go-github-com-stretchr-testify
+                             go-github-com-pkg-errors
+                             go-github-com-google-flatbuffers
+                             go-github-com-golang-protobuf
+                             go-github-com-gogo-protobuf
+                             go-github-com-chewxy-math32
+                             go-github-com-chewxy-hm
+                             go-github-com-apache-arrow-go-arrow))
+    (home-page "https://gorgonia.org/tensor")
+    (synopsis "Package")
+    (description
+     "Package tensor is a package that provides efficient, generic n-dimensional
+arrays in Go.  Also in this package are functions and methods that are used
+commonly in arithmetic, comparison and linear algebra operations.")
+    (license license:asl2.0)))
+
+(define-public go-github-com-dlclark-regexp2-v1.11.5
+  (package
+    (name "go-github-com-dlclark-regexp2")
+    (version "1.11.5")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/dlclark/regexp2")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0i5c7ak8r4wwlyrx5f1mdipqk6p6ms1jgclb7hlb4qgy83c7xplc"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "github.com/dlclark/regexp2"))
+    (home-page "https://github.com/dlclark/regexp2/")
+    (synopsis "Full featured regular expressions for Go")
+    (description "Regexp2 is a feature-rich RegExp engine for Go.")
+    (license license:expat)))
