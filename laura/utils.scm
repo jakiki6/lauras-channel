7c0ca05917ff46bb99d7c42374abc7848d65c659
Jakob Kirsch <jakob.kirsch@web.de>
2025-03-17 16:20:21 +0100

    add utils

---
+++ laura/utils.scm
@@ -0,0 +1,30 @@
+(define-module (laura utils)
+  #:use-module (ice-9 popen)
+  #:use-module (ice-9 textual-ports))
+
+(define (http-get uri)
+  (let* ((port (open-input-pipe (string-append "curl -sL " uri)))
+         (body (get-string-all port)))
+    (begin
+      (close-port port) body)))
+
+(define-public (patched-upstream-guix issues)
+  (let* ((urls (map (lambda (issue)
+                      (string-append "https://issues.guix.gnu.org/issue/"
+                                     (number->string (car issue))
+                                     "/attachment/"
+                                     (string-join (map number->string
+                                                       (cdr issue)) "/")))
+                    issues))
+         (patches (map (lambda (url)
+                         (http-get url)) urls)))
+    (lambda (checkout)
+      (let ((cwd (getcwd)))
+        (begin
+          (chdir checkout)
+          (map (lambda (patch)
+                 (let ((port (open-output-pipe "patch -p1")))
+                        (put-string port patch)
+                        (close-port port))
+                   ) patches)
+          (chdir cwd))))))
