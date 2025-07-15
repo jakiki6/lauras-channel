7709634c6232a299b3417084fc925ec798c8cadb
Jakob Kirsch <jakob.kirsch@web.de>
2025-07-15 04:08:06 +0200

    add u8_r2_plugin

--- laura/packages/utils.scm
+++ laura/packages/utils.scm
@@ -72,6 +72,7 @@
   #:use-module (gnu packages sagemath)
   #:use-module (gnu packages base)
   #:use-module (gnu packages crates-database)
+  #:use-module (gnu packages engineering)
   #:use-module (gnu system uuid)
   #:use-module (guix build utils)
   #:use-module (guix build-system cmake)
@@ -2175,3 +2176,34 @@ ssh-ed25519 keys into X25519 keys preserving keypair correspondence.")
     (description
      "sqlite-fs allows Linux and MacOS to mount a sqlite database file as a normal filesystem.")
     (license license:expat)))
+
+(define-public u8_r2_plugin
+  (package
+    (name "u8_r2_plugin")
+    (version "0.0.1")
+    (source
+      (origin
+        (method git-fetch)
+        (uri (git-reference
+              (url "https://github.com/cetus9/u8_r2_plugin")
+              (commit "7bedc13")))
+        (file-name (git-file-name name version))
+(patches (search-patches
+                          "laura/packages/patches/u8_r2_plugin.patch"))
+        (sha256 (base32 "0affzh6c9ybzircmsvqahvryspc32nq7n3f3pqa6kql8zp7az6gf"))))
+    (build-system gnu-build-system)
+    (native-inputs (list pkg-config))
+    (inputs (list radare2 openssl))
+    (arguments (list #:tests? #f
+#:phases #~(modify-phases %standard-phases
+(replace 'configure (lambda _
+  (setenv "CC" "gcc")))
+(replace 'install (lambda _
+(mkdir-p (string-append #$output "/share/radare2/plugins"))
+(copy-file "asm_u8.so" (string-append #$output "/share/radare2/plugins/asm_u8.so"))
+(copy-file "anal_u8.so" (string-append #$output "/share/radare2/plugins/anal_u8.so"))
+)))))
+    (home-page "https://github.com/cetus9/u8_r2_plugin")
+    (synopsis "radare2 plugins for nX-U8/100")
+    (description "Experimental radare2 disassembly and analysis plugins for nX-U8/100 architecture.")
+    (license license:lgpl2.1)))
