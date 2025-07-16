f467abd55b98f19238ab5dc966142d5591e7c718
Jakob Kirsch <jakob.kirsch@web.de>
2025-07-17 00:34:19 +0200

    add acarsdec

--- laura/packages/utils.scm
+++ laura/packages/utils.scm
@@ -73,6 +73,7 @@
   #:use-module (gnu packages base)
   #:use-module (gnu packages crates-database)
   #:use-module (gnu packages engineering)
+  #:use-module (gnu packages radio)
   #:use-module (gnu system uuid)
   #:use-module (guix build utils)
   #:use-module (guix build-system cmake)
@@ -2182,28 +2183,65 @@ ssh-ed25519 keys into X25519 keys preserving keypair correspondence.")
     (name "u8_r2_plugin")
     (version "0.0.1")
     (source
-      (origin
-        (method git-fetch)
-        (uri (git-reference
-              (url "https://github.com/cetus9/u8_r2_plugin")
-              (commit "7bedc13")))
-        (file-name (git-file-name name version))
-(patches (search-patches
-                          "laura/packages/patches/u8_r2_plugin.patch"))
-        (sha256 (base32 "0affzh6c9ybzircmsvqahvryspc32nq7n3f3pqa6kql8zp7az6gf"))))
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/cetus9/u8_r2_plugin")
+             (commit "7bedc13")))
+       (file-name (git-file-name name version))
+       (patches (search-patches "laura/packages/patches/u8_r2_plugin.patch"))
+       (sha256
+        (base32 "0affzh6c9ybzircmsvqahvryspc32nq7n3f3pqa6kql8zp7az6gf"))))
     (build-system gnu-build-system)
     (native-inputs (list pkg-config))
     (inputs (list radare2 openssl))
-    (arguments (list #:tests? #f
-#:phases #~(modify-phases %standard-phases
-(replace 'configure (lambda _
-  (setenv "CC" "gcc")))
-(replace 'install (lambda _
-(mkdir-p (string-append #$output "/share/radare2/plugins"))
-(copy-file "asm_u8.so" (string-append #$output "/share/radare2/plugins/asm_u8.so"))
-(copy-file "anal_u8.so" (string-append #$output "/share/radare2/plugins/anal_u8.so"))
-)))))
+    (arguments
+     (list
+      #:tests? #f
+      #:phases
+      #~(modify-phases %standard-phases
+          (replace 'configure
+            (lambda _
+              (setenv "CC" "gcc")))
+          (replace 'install
+            (lambda _
+              (mkdir-p (string-append #$output "/share/radare2/plugins"))
+              (copy-file "asm_u8.so"
+                         (string-append #$output
+                                        "/share/radare2/plugins/asm_u8.so"))
+              (copy-file "anal_u8.so"
+                         (string-append #$output
+                                        "/share/radare2/plugins/anal_u8.so")))))))
     (home-page "https://github.com/cetus9/u8_r2_plugin")
     (synopsis "radare2 plugins for nX-U8/100")
-    (description "Experimental radare2 disassembly and analysis plugins for nX-U8/100 architecture.")
+    (description
+     "Experimental radare2 disassembly and analysis plugins for nX-U8/100 architecture.")
     (license license:lgpl2.1)))
+
+(define-public acarsdec
+  (package
+    (name "acarsdec")
+    (version "3.7")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/TLeconte/acarsdec")
+             (commit "7920079")))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0wlrvn81xfzpnz9m1m9cgjiczpfjzj23l1na61m9kj5d62c1lks8"))))
+    (build-system cmake-build-system)
+    (native-inputs (list pkg-config))
+    (inputs (list libacars rtl-sdr))
+    (arguments
+     (list
+      #:tests? #f
+      #:build-type "Release"
+      #:configure-flags
+      #~(list "-Drtl=on")))
+    (home-page "https://github.com/TLeconte/acarsdec")
+    (synopsis "ACARS SDR decoder")
+    (description
+     "Acarsdec is a multi-channels acars decoder with built-in rtl_sdr, airspy front end or sdrplay device. Since 3.0, It comes with a database backend : acarsserv to store received acars messages.")
+    (license license:gpl2)))
